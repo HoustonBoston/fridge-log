9cf68c8c93aeb1e74ddef29b2629699f44589241
HoustonBoston <myhorsefly12345@gmail.com>
2024-12-20 09:02:50 -0500

    If difference is 1 then we can say it will be expiring soon.

--- src/components/pages/page.js
+++ src/components/pages/page.js
@@ -85,7 +85,7 @@ export default function LaptopPage({ setIsAuthenticated }) {
             user_email: userEmail,
             item_id: uuidv4(),
             item_name: "",
-            expiry_date: currentDate.add(1, "day").unix(),
+            expiry_date: currentDate.add(2, "day").unix(),
             purchase_date: currentDate.unix(),
             timestamp: dayjs().unix()
         }
