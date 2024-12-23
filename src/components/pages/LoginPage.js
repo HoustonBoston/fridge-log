225d933b3875acf1c62c617e72afd331f78aff3c
HoustonBoston <myhorsefly12345@gmail.com>
2024-12-22 20:51:20 -0500

    incomplete

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -6,7 +6,26 @@ import { Box } from "@mui/material"
 export default function LoginPage({ setIsAuthenicated }) {
     const navigate = useNavigate()
 
-    const handleLoginSuccess = (credentialResponse) => {
+    // call api to check if email is already in DB
+    const checkEmailInDDB = async () => {
+        const decoded = jwtDecode(localStorage.getItem('user_token'))
+        const userEmail = decoded.email
+        const apiUrl = `http://${device_ip}:8080/CheckEmailExistence` //api gw url, can be accessed via host machine's IP with configured firewall
+        console.log('trying to call fetch items API')
+        try{
+            const res = await fetch(apiUrl, {
+                method: 'GET',
+                body: JSON.stringify({
+                    email: userEmail
+                })
+            })
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    const handleLoginSuccess = async (credentialResponse) => {
+        await checkEmailInDDB()
         localStorage.setItem('user_token', credentialResponse.credential)
         setIsAuthenicated(true)
         navigate('/')
