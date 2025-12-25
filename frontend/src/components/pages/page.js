b0ccffec1ea67b6df902a22ff73de6d82783368d
HoustonBoston <myhorsefly12345@gmail.com>
2025-12-25 08:08:11 -0500

    Tested to see if setting fridge items after performing animation would change anything

--- frontend/src/components/pages/page.js
+++ frontend/src/components/pages/page.js
@@ -167,7 +167,6 @@ export default function LaptopPage ({ setIsAuthenticated })
                 onComplete: () => {
                     // After fade out, remove from state and animate remaining items
                     setFridgeItems((prevItems) => {
-                        const newItems = prevItems.filter((item) => item.item_id !== id)
                         // Schedule Flip animation for next render
                         requestAnimationFrame(() => {
                             if (listRef.current) {
@@ -178,6 +177,7 @@ export default function LaptopPage ({ setIsAuthenticated })
                                 })
                             }
                         })
+                        const newItems = prevItems.filter((item) => item.item_id !== id)
                         return newItems
                     })
                 }
