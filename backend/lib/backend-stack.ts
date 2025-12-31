74bcd2bc08b5f757dcb5f3c2fdcf021f8ef3135c
HoustonBoston <myhorsefly12345@gmail.com>
2025-12-31 16:15:18 -0500

    added one of the tables to stack

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,11 +1,17 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import { aws_dynamodb } from 'aws-cdk-lib';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // DynamoDB
+    const fridgeItemsTable = new aws_dynamodb.TableV2(this, 'FridgeLogItem', {
+      partitionKey: {name: 'user_email', type: aws_dynamodb.AttributeType.STRING},
+      sortKey: {name: 'timestamp', type: aws_dynamodb.AttributeType.NUMBER}
+    })
+    fridgeItemsTable.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN)  // prevent deletion on stack removal
 
     // Lambdas
 
