dc632717504ebf6ce5f9a3573153b3ec9eb4fed5
HoustonBoston <myhorsefly12345@gmail.com>
2026-01-01 11:07:18 -0500

    Added Lambdas, work on API GW Lambda proxy

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-import { aws_dynamodb } from 'aws-cdk-lib';
+import { aws_dynamodb, aws_lambda_nodejs, aws_apigateway } from 'aws-cdk-lib';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import path from 'path';
+
+const LAMBDA_PATH = '../lambdas'
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,8 +18,51 @@ export class BackendStack extends cdk.Stack {
     fridgeItemsTable.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN)  // prevent deletion on stack removal
 
     // Lambdas
+    new aws_lambda_nodejs.NodejsFunction(this, 'CapturePhoto', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, LAMBDA_PATH + '/CapturePhoto/capture_photo.mjs'),
+      handler: 'handler',
+      timeout: cdk.Duration.seconds(15)
+    })
+
+    new aws_lambda_nodejs.NodejsFunction(this, 'CheckEmailExistence', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, LAMBDA_PATH + '/CheckEmailExistence/check_email_existence.mjs'),
+      handler: 'handler',
+      timeout: cdk.Duration.seconds(15)
+    })
+
+    new aws_lambda_nodejs.NodejsFunction(this, 'DeleteItemDDB', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, LAMBDA_PATH + '/DeleteItemDDB/delete_item.mjs'),
+      handler: 'handler',
+      timeout: cdk.Duration.seconds(15)
+    })
+
+    new aws_lambda_nodejs.NodejsFunction(this, 'ReadDDB', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, LAMBDA_PATH + '/ReadDDB/read_from_ddb.mjs'),
+      handler: 'handler',
+      timeout: cdk.Duration.seconds(15)
+    })
+
+    new aws_lambda_nodejs.NodejsFunction(this, 'ScanSendNotif', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, LAMBDA_PATH + '/ScanSendNotif/scan_send_notif.mjs'),
+      handler: 'handler',
+      timeout: cdk.Duration.seconds(15)
+    })
+
+    new aws_lambda_nodejs.NodejsFunction(this, 'WriteDDB', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, LAMBDA_PATH + '/WriteDDB/write_to_ddb.mjs'),
+      handler: 'handler',
+      timeout: cdk.Duration.seconds(15)
+    })
+
 
     // API Gateway
+    new aws_apigateway.LambdaIntegration
 
     // EventBridge
 
