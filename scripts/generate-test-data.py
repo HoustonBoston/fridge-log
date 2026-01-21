23826d2e24922a5088fa09766780f92da6ac02e5
HoustonBoston <myhorsefly12345@gmail.com>
2026-01-21 08:08:14 -0500

    Boilerplate python code

---
+++ scripts/generate-test-data.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+all_produce = [
+    'Apple', 'Banana', 'Orange', 'Mango', 'Strawberry', 'Blueberry', 'Raspberry', 'Grapes', 'Pineapple', 'Watermelon',
+    'Cantaloupe', 'Honeydew', 'Kiwi', 'Lemon', 'Lime', 'Peach', 'Plum', 'Cherry', 'Pear', 'Avocado',
+    'Tomato', 'Cucumber', 'Carrot', 'Broccoli', 'Spinach', 'Kale', 'Lettuce', 'Onion', 'Garlic', 'Potato',
+    'Sweet Potato', 'Bell Pepper', 'Zucchini', 'Eggplant', 'Asparagus', 'Celery', 'Peas', 'Green Beans', 'Corn', 'Cabbage',
+    'Cauliflower', 'Mushroom', 'Artichoke', 'Radish', 'Beetroot', 'Pumpkin', 'Squash', 'Okra', 'Ginger', 'Turmeric',
+    'Arugula', 'Endive', 'Fennel', 'Parsnip', 'Turnip', 'Yam', 'Jalapeño', 'Olive', 'Fig', 'Date',
+    'Pomegranate', 'Papaya', 'Guava', 'Passion Fruit', 'Dragon Fruit', 'Lychee', 'Rambutan', 'Star Fruit', 'Coconut', 'Apricot',
+    'Cranberry', 'Blackberry', 'Lime', 'Orange', 'Grapefruit', 'Lemon']
+
+# Send request to add to DynamoDB
+for item in all_produce:
+    try:
+        
+    except:
